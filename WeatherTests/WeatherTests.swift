16e536d88f63f130b814b6aed349024b55e0c123
Vikramaditya Gaikwad <em24mtech14008@iith.ac.in>
2025-02-11 23:36:14 +0530

    Added Test Files

---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Vikramaditya Gaikwad on 11/02/25.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
